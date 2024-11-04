7ac3e2ac2ad6a7a06aec529b42fcd981953aa73a
mcnoelvito <noelvitohu@gmail.com>
2024-11-04 09:32:13 +0700

    Portofolio in bottom nav

--- BottomNav.js
+++ BottomNav.js
@@ -2,12 +2,11 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Portofolio from './App';
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
+      <Portofolio/>
   );
 }
 
