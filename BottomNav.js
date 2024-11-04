0c62cf5e00fbd917ac0f45818a42fa4229dfaf63
mcnoelvito <noelvitohu@gmail.com>
2024-11-04 10:01:28 +0700

    add data mahasiswa

--- BottomNav.js
+++ BottomNav.js
@@ -3,6 +3,8 @@ import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Portofolio from './App';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faUser, faUserGraduate } from '@fortawesome/free-solid-svg-icons'
 
 function HomeScreen() {
   return (
@@ -24,8 +26,19 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Profil" component={HomeScreen} 
+        options={{ 
+          headerShown: false,
+          tabBarIcon: ({color}) => 
+         ( <FontAwesomeIcon icon={faUser} color={color} size={24}/> ),
+        }}/>
+        <Tab.Screen name="Data Mahasiswa" component={SettingsScreen} 
+        options={{ 
+          headerShown: false,
+          tabBarIcon: ({color}) => 
+         ( <FontAwesomeIcon icon={faUserGraduate} color={color} size={24}/> ),
+        }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
